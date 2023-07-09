e601d39c3acf68b09fee4c5499ae8dea192bb2bb
streeese <94462388+streeese@users.noreply.github.com>
2023-07-09 13:18:42 -0400

    Test Agregar

--- tu-mesa/src/app/agregar/agregar.page.spec.ts
+++ tu-mesa/src/app/agregar/agregar.page.spec.ts
@@ -1,11 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AgregarPage } from './agregar.page';
+import { SQLite } from '@ionic-native/sqlite';
 
 describe('AgregarPage', () => {
   let component: AgregarPage;
   let fixture: ComponentFixture<AgregarPage>;
 
   beforeEach(async() => {
+    TestBed.configureTestingModule({
+      providers: [SQLite],
+    });
     fixture = TestBed.createComponent(AgregarPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
