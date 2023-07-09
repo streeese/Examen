91b4038a8b4853722e4566a1efaf615e27c14fe3
streeese <94462388+streeese@users.noreply.github.com>
2023-07-09 13:11:04 -0400

    test api

--- tu-mesa/src/app/services/apiservice.service.spec.ts
+++ tu-mesa/src/app/services/apiservice.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiserviceService } from './apiservice.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 describe('ApiserviceService', () => {
   let service: ApiserviceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HttpClient],
+    });
     service = TestBed.inject(ApiserviceService);
   });
 
