9df965e93e6afc74e965352693806e6d2cf57879
streeese <94462388+streeese@users.noreply.github.com>
2023-07-10 11:53:13 -0400

    Test

--- tu-mesa/src/app/app.component.spec.ts
+++ tu-mesa/src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
 
@@ -11,6 +12,58 @@ describe('AppComponent', () => {
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
+
+  
+  it('Debe existir el AppComponent', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy(); //TODO: ✔
+  });
+
+  it('Debe retornar formulario invalido', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance
+    fixture.detectChanges() 
+
+    const email = app.login.controls['email']
+    email.setValue('leker33@gmail.com')
+
+    expect(app.login.invalid).toBeTrue(); 
+  });
+
+  it('Debe retornar formulario valido', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance
+    fixture.detectChanges()
+
+    let email = app.login.controls['email']
+    let password = app.login.controls['password']
+    let result = app.login.controls['result']
+
+    email.setValue('leker33@gmail.com')
+    password.setValue('123456')
+    result.setValue('1')
+
+
+    expect(app.login.invalid).toBeFalse();
+  });
+
+  it(`Debe actulizar datos de usuario`, () => {
+       const fixture = TestBed.createComponent(AppComponent);
+       const app = fixture.componentInstance;
+       fixture.detectChanges()
+  
+       let email = app.login.controls['email']
+       let password = app.login.controls['password']
+  
+       email.setValue('leker33@gmail.com')
+       password.setValue('123456')
+  
+       const btnElement = fixture.debugElement.query(By.css('button.btn'))
+       btnElement.nativeElement.click()
+       const testData = { user: 1 }
+       expect(app.isCheck).toEqual(testData)
+     });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- tu-mesa/src/app/services/apiservice.service.spec.ts
+++ tu-mesa/src/app/services/apiservice.service.spec.ts
@@ -3,8 +3,8 @@ import { TestBed } from '@angular/core/testing';
 import { ApiserviceService } from './apiservice.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
-import { AppComponent } from '../app.component';
-import { By } from '@angular/platform-browser';
+
+
 
 describe('ApiserviceService', () => {
   let service: ApiserviceService;
@@ -16,58 +16,6 @@ describe('ApiserviceService', () => {
     });
     service = TestBed.inject(ApiserviceService);
   });
-
-   
-  it('Debe existir el AppComponent', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance
-    expect(app).toBeTruthy(); //TODO: ✔
-  });
-
-  it('Debe retornar formulario invalido', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance
-    fixture.detectChanges() 
-
-    const email = app.login.controls['email']
-    email.setValue('leker33@gmail.com')
-
-    expect(app.login.invalid).toBeTrue(); 
-  });
-
-  it('Debe retornar formulario valido', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance
-    fixture.detectChanges()
-
-    let email = app.login.controls['email']
-    let password = app.login.controls['password']
-    let result = app.login.controls['result']
-
-    email.setValue('leker33@gmail.com')
-    password.setValue('123456')
-    result.setValue('1')
-
-
-    expect(app.login.invalid).toBeFalse();
-  });
-
-  it(`Debe actulizar datos de usuario`, () => {
-       const fixture = TestBed.createComponent(AppComponent);
-       const app = fixture.componentInstance;
-       fixture.detectChanges()
-  
-       let email = app.login.controls['email']
-       let password = app.login.controls['password']
-  
-       email.setValue('leker33@gmail.com')
-       password.setValue('123456')
-  
-       const btnElement = fixture.debugElement.query(By.css('button.btn'))
-       btnElement.nativeElement.click()
-       const testData = { user: 1 }
-       expect(app.isCheck).toEqual(testData)
-     });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
